5a48d3fdfc00e0165e4400e53116b667e46ffb5e
Diptavo Dutta <dutta.diptavo@gmail.com>
2020-12-16 18:17:48 -0500

    update packages and codes

--- GAUSS/R/GAUSS_All.R
+++ GAUSS/R/GAUSS_All.R
@@ -1,4 +1,4 @@
-GAUSS_All <- function(summary_file, gene_name, pv_name,output_file,gmt,verbose = TRUE,ags = "def",parallel = FALSE,jobs,jobfile,start=NA,stop=NA,is.appx = TRUE){
+GAUSS_All <- function(summary_file, gene_name, pv_name,output_file,gmt,verbose = TRUE,method = "SKAT-CR",ags = "def",parallel = FALSE,jobs,jobfile,start=NA,stop=NA,gpd.est.res = NULL, is.appx = TRUE){
   
   LOGF<- paste0(output_file,".log")
   OUTF <-paste0(output_file,".out") 
@@ -12,9 +12,19 @@ GAUSS_All <- function(summary_file, gene_name, pv_name,output_file,gmt,verbose =
   }else{
     cat(paste0("No approximations invoked. Only resampling p-values of limited precision will be obtained... \n"),file = LOGF,append = T)
   }
+  
+  
+  met1 <- grep("TWAS",method);
+  if(length(met1) == 0){
+    data("SKAT_CR", package = "GAUSS"); print("Gene-based p-values from SKAT-CR test.."); cat("Gene-based p-values from SKAT-CR test...\n\n",file = LOGF,append = T)
+  }else{
+    tissue <- strsplit(method,split = "TWAS.")[[1]][2];
+    datf <- paste0(method); 
+    data(list = datf,package = "GAUSS"); print(paste0("Gene-based p-values from TWAS-FUSION test in ",tissue," tissue ..")); 
+    cat(paste0("Gene-based p-values from TWAS-FUSION test in ",tissue," tissue ...\n\n"),file = LOGF,append = T);
+  }
   print("loading ref data...")
   cat("loading ref data ...\n\n",file = LOGF,append = T)
-  print(head(colnames(pv.null.wt1)))
   
   if(ags == "def"){
     ags = c(1e+05,250,100,-10,500,0.05)
@@ -23,6 +33,22 @@ GAUSS_All <- function(summary_file, gene_name, pv_name,output_file,gmt,verbose =
   }else{
     ags <- read.table(ags)[,1]
   }
+  if(!is.null(gpd.est.res)){
+    ags[2] <- gpd.est.res;
+  }
+  print(paste0("Number of resamples for GPD approximation is ",ags[2]))
+  cat(paste0("Number of resamples for GPD approximation is ",ags[2],"\n"),file = LOGF,append = T)
+  
+  print(paste0(dim(pv.null.wt1)[2]," genes in the reference panel"))
+  cat(paste0(dim(pv.null.wt1)[2]," genes in the reference panel \n\n"),file = LOGF,append = T)
+  
+  
+  a1 <- read.table(summary_file); gns <- a1[,ggg]; 
+  gns.cmn <- length(intersect(colnames(pv.null.wt1),gns))
+
+  print(paste0(gns.cmn," genes in common between the reference panel and genes in the input file"))
+  cat(paste0(gns.cmn," genes in common between the reference panel and genes in the input file \n\n"),file = LOGF,append = T)
+  
   
   s1 <- read.table(gmt,header = T); summary_file = read.table(summary_file);
   if(parallel){
@@ -36,7 +62,7 @@ GAUSS_All <- function(summary_file, gene_name, pv_name,output_file,gmt,verbose =
   s1 <- s1[c(start:stop),]
   print(paste0("reading summary and gmt files... "))
   cat("reading summary and gmt files \n\n",file = LOGF,append = T)
-  pv <- NULL;
+  pv <- NULL; excl <- NULL; lp <- NULL
   cs <- list();
   set.seed(3721)
   
@@ -55,15 +81,15 @@ GAUSS_All <- function(summary_file, gene_name, pv_name,output_file,gmt,verbose =
       cat(paste0("\n"), file = LOGF,append = T)
     }
     
-    rt1 <- system.time(t1 <- gauss(gene.set= as.character(s1[i,3]),inf = summary_file,ggg = gene_name,cols1 = pv_name,max.prec = ags[1], ct=ags[2],ct.min = ags[3],stp = ags[4],tl = ags[5],thr = ags[6],verbose = verbose,LOGF = LOGF,is.appx = is.appx))
-    pv <- c(pv,t1$pvalue); cs[[i]] <- t1$CS;
+    rt1 <- system.time(t1 <- gauss(gene.set= as.character(s1[i,3]),inf = summary_file,ggg = gene_name,cols1 = pv_name,max.prec = ags[1], ct=ags[2],ct.min = ags[3],stp = ags[4],tl = ags[5],thr = ags[6],verbose = verbose,LOGF = LOGF))
+    pv <- c(pv,t1$pvalue); cs[[i]] <- t1$CS; excl <- c(excl,t1$excl); lp <- c(lp,t1$length);
     cat(paste0("Analysis done in ",round(as.numeric(rt1[3]),4)," seconds \n\n"),file = LOGF, append = T)
-    write.table(data.frame(s1[1:i,1],pv[1:i],I(unlist(lapply(cs,paste,collapse=",")))),OUTF,col.names = c("GeneSet","pvalue","CS"),row.names = F,quote = F);
+    write.table(data.frame(s1[1:i,1],lp[1:i],pv[1:i],I(unlist(lapply(cs,paste,collapse=","))),excl[1:i]),OUTF,col.names = c("GeneSet","Length_GS","pvalue","CS","excluded"),row.names = F,quote = F);
   })
   
   cat(paste0("\n\n"), file = LOGF,append = T)
   cat(paste0("Analyzed ",i," gene-sets in ",round(as.numeric(aaa[3]/60),5)," minutes \n\n"), file = LOGF, append = T)
   sink(type = "message")
   close(ll)
-  
+  closeAllConnections();
 }
--- GAUSS/R/gauss.R
+++ GAUSS/R/gauss.R
@@ -1,44 +1,56 @@
 gauss <-
-function(gene.set,inf,ggg,cols1,max.prec = 1e+05, ct=250,ct.min = 100,stp = -10,tl = 500,thr = 0.05,verbose = T,LOGF = "out.log"){
-  
-  inf = inf[match(colnames(pv.null.wt1),inf[,ggg]),]  
-  gene.set <- gsub(c(" "), "",gene.set); gene.set <- unique(gene.set)
-  gene.set <- strsplit(as.character(gene.set),split = ",")[[1]]
-  length.path <- length(gene.set)
-  if(verbose)
-   cat("Preprocessing \n",file = LOGF,append = T)
-  
-  common <- intersect(gene.set,colnames(pv.null.wt1))
-  gs1 <- gene.set[match(common,gene.set)]
-  cl1 <- colnames(pv.null.wt1)[match(common,colnames(pv.null.wt1))]
-  
-  z.mat <- as.matrix(pv.null.wt1[,colnames(pv.null.wt1) %in% cl1])
-  
-  if(length(z.mat) > 0){
-    cor.z = cor(as.matrix(z.mat),use = "pairwise.complete.obs");
+  function(gene.set,inf,ggg,cols1,max.prec = 1e+05, ct=250,ct.min = 100,stp = -10,tl = 500,thr = 0.05,verbose = T,LOGF = "out.log"){
     
-    pv.mat <- inf[match(common,colnames(pv.null.wt1)),]
-    pv1 <- pv.mat[,cols1]
+    inf = inf[match(colnames(pv.null.wt1),inf[,ggg]),]; inf <- inf[!is.na(inf[,cols1]),]  
+    gene.set <- gsub(c(" "), "",gene.set); gene.set <- unique(gene.set)
+    gene.set <- strsplit(as.character(gene.set),split = ",")[[1]]
+    length.path <- length(gene.set)
+    if(verbose)
+      cat("Preprocessing \n",file = LOGF,append = T)
     
-    p.noinf = pv1 != 1
-    
-    if(length(p.noinf) > 0){
-      pv2 = pv1[p.noinf]
-      cor.z = as.matrix(cor.z[p.noinf,p.noinf])
+    common <- intersect(gene.set,colnames(pv.null.wt1)); excl <- length.path - length(common);
+    if(length(common) > 0){
+      gs1 <- gene.set[match(common,gene.set)]
+      cl1 <- colnames(pv.null.wt1)[match(common,colnames(pv.null.wt1))]
       
-      pv.mat2 <- pv.mat[p.noinf,]
-      diag(cor.z) = 1; cor.z[is.na(cor.z)] = 0; cor.z[is.nan(cor.z)] = 0;
+      z.mat <- as.matrix(pv.null.wt1[,colnames(pv.null.wt1) %in% cl1])
       
-      if(sum(p.noinf,na.rm = T) > 0){
-        test.obj <- Cal_S_EqualWeight(qnorm(pv2,lower.tail = F))
-        stat.wt <- test.obj$maxS
-        gene.idx.wt <- pv.mat2[test.obj$idx,1]
-        p1 <- Calc.PV(stat.wt,cor.z,max.prec = max.prec, ct=ct,ct.min = ct.min,stp = stp,tl = tl,thr = thr,verbose = verbose,LOGF = LOGF,is.appx = is.appx)
-        pval.wt <- p1$pval; gpd.used = as.numeric(p1$gpd.used)
+      if(length(z.mat) > 0){
+        cor.z = cor(as.matrix(z.mat),use = "pairwise.complete.obs");
+        
+        pv.mat <- inf[match(common,colnames(pv.null.wt1)),]
+        pv1 <- pv.mat[,cols1]
+        if(sum(is.na(pv1)) == length(pv1)){
+          return(list("pvalue" = NA, "CS" = NA, "Test.stat" = NA,"gpd.used" = NA,"excl" = excl, "length" = common))
+        }else{
+          p.noinf = pv1 != 1
+          
+          if(sum(p.noinf,na.rm = T) > 0){
+            pv2 = pv1[p.noinf]
+            cor.z = as.matrix(cor.z[p.noinf,p.noinf])
+            
+            pv.mat2 <- pv.mat[p.noinf,]
+            diag(cor.z) = 1; cor.z[is.na(cor.z)] = 0; cor.z[is.nan(cor.z)] = 0;
+            
+            if(sum(p.noinf,na.rm = T) > 0){
+              test.obj <- Cal_S_EqualWeight(qnorm(pv2,lower.tail = F))
+              stat.wt <- test.obj$maxS
+              gene.idx.wt <- pv.mat2[test.obj$idx,1]
+              p1 <- Calc.PV(stat.wt,cor.z,max.prec = max.prec, ct=ct,ct.min = ct.min,stp = stp,tl = tl,thr = thr,verbose = verbose,LOGF = LOGF,is.appx = is.appx)
+              pval.wt <- p1$pval; gpd.used = as.numeric(p1$gpd.used)
+            }
+            
+            gauss.obj <- list("pvalue" = pval.wt, "CS" = gene.idx.wt, "Test.stat" = stat.wt,"gpd.used" = gpd.used, "excl" = excl, "length" = length.path);
+            return(gauss.obj)
+          }else{
+            return(list("pvalue" = NA, "CS" = NA, "Test.stat" = NA,"gpd.used" = NA,"excl" = excl, "length" = common))
+          }
+        }
+      }else{
+        return(list("pvalue" = NA, "CS" = NA, "Test.stat" = NA,"gpd.used" = NA,"excl" = excl, "length" = common))
       }
       
-      gauss.obj <- list("pvalue" = pval.wt, "CS" = gene.idx.wt, "Test.stat" = stat.wt,"gpd.used" = gpd.used);
-      return(gauss.obj)
+    }else{
+      return(list("pvalue" = NA, "CS" = NA, "Test.stat" = NA,"gpd.used" = NA,"excl" = excl, "length" = common))
     }
   }
-}
